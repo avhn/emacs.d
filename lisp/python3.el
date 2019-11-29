20988a833cf86de5df7ca8ce595ece3a4eec0d1f
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-11-30 01:31:21 +0300

    Refactor maps.el to keymaps.el

--- lisp/python3.el
+++ lisp/python3.el
@@ -1,7 +1,8 @@
 (add-hook 'python-mode-hook (lambda ()
   (setq indent-tabs-mode nil
-		python-indent 4)  
-  ;; use virtualenv for elpy
+		python-indent 4)
+
+  ;; use python 3.*
   (setq elpy-rpc-python-command "python3")
   (elpy-enable)
 ))
