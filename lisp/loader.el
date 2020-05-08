8b5cc71e951569044bdb33aa0d3d20e4ac567ad5
Anaxilaus <mertdede@protonmail.com>
2020-05-08 12:22:05 +0300

    Use microsoft language server for python

--- lisp/loader.el
+++ lisp/loader.el
@@ -16,19 +16,12 @@
   :commands (lsp lsp-deferred)
   :hook (go-mode . lsp-deferred))
 
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-;; Optional - provides fancier overlays.
+;; provides fancier overlays.
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode)
 
-;; Company mode is a standard completion package that works well with lsp-mode.
+;; company mode is a standard completion package that works well with lsp-mode.
 (use-package company
   :ensure t
   :config
@@ -42,25 +35,41 @@
   :ensure t
   :commands company-lsp)
 
-;; Optional - provides snippet support.
+;; provides snippet support.
 (use-package yasnippet
   :ensure t
   :commands yas-minor-mode
   :hook (go-mode . yas-minor-mode))
 
+;; modes
+
+;; set up before-save hooks to format buffer and add/delete imports.
+;; make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
 (use-package go-mode
   :ensure t)
+
+(use-package lsp-python-ms
+  :ensure t
+  :hook python-mode
+  :config
+  (require 'lsp-python-ms)
+  (lsp))
 
 (use-package markdown-mode
   :ensure t
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (lambda()
-          ;;; use pandoc
-          (setq markdown-command "pandoc")
-          (auto-fill-mode)
-          (setq fill-column 90)))
+  :init
+  ;;; use pandoc
+  (setq markdown-command "pandoc")
+  (auto-fill-mode)
+  (setq fill-column 90))
 
 ;;; bindings
 
