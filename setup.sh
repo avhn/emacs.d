fc915911154f2115526defc9102776f169ce08c4
Anaxilaus <mertdede@protonmail.com>
2020-05-08 06:28:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,17 +9,16 @@ else
     echo "Skipping copying (there's already a file or directory exists at $HOME/.emacs.d)"
 fi
 
-if [ -f $HOME/.emacs.d/install_libraries.el ]; then
-    emacs -nw --load $HOME/.emacs.d/install_libraries.el
+# install python lsp
+if [[ "$(which pip3)" != "" ]]; then
+    pip3 install 'python-language-server[all]'
 else
-    echo "${RED}No suitable .el file found to install libraries"
+    echo "${RED}pip3 not found in path"
 fi
 
-# install golang binaries
+# install golang lsp
 if [[ "$(which go)" != "" ]]; then # check path
-    go get golang.org/x/tools/cmd/...
-    go get github.com/rogpeppe/godef
-    go get -u github.com/nsf/gocode # go-autocomplete binaries
+   go get -u golang.org/x/tools/gopls
 else
     echo "${RED}Golang binary not found in path"
 fi
@@ -31,5 +30,8 @@ if [[ ! $(which pandoc) ]]; then
         echo "${RED}pandoc not installed, install for markdown support"
     fi
 fi
+
+# initial launch to install packages
+emacs -nw --eval "(save-buffers-kill-emacs)"
 
 echo "Done"
